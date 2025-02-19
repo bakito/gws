7664e6706527983f0d00b177b2b9d7b5e5055098
bakito <github@bakito.ch>
2025-02-19 10:13:04 +0100

    select contrxt

--- cmd/ctx.go
+++ cmd/ctx.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/bakito/gws/pkg/types"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
+	"maps"
+	"slices"
 )
 
 // ctxCmd represents the ctx command
@@ -16,11 +21,74 @@ var ctxCmd = &cobra.Command{
 		if len(args) == 1 {
 			return cfg.SwitchContext(args[0])
 		}
-		// TODO print available contexts
+		selected, err := selectContext(cfg)
+		if err != nil {
+			return err
+		}
+
+		if selected != "" {
+			return cfg.SwitchContext(selected)
+		}
+
 		return nil
 	},
 }
 
+func selectContext(cfg *types.Config) (string, error) {
+	m := ctxModel{choices: slices.Sorted(maps.Keys(cfg.Contexts))}
+	p := tea.NewProgram(m)
+
+	if _, err := p.Run(); err != nil {
+		return "", err
+	}
+	return m.selected, nil
+}
+
 func init() {
 	rootCmd.AddCommand(ctxCmd)
+}
+
+type ctxModel struct {
+	cursor   int
+	choices  []string
+	selected string
+}
+
+func (m ctxModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m ctxModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		case "up":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter":
+			m.selected = m.choices[m.cursor]
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m ctxModel) View() string {
+	s := "Choose an option:\n\n"
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+	s += "\nPress ↑/↓ to move, Enter to select, Q to quit.\n"
+	return s
 }
