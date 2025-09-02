ac8395c0140785bcdd879d076649ed4b65a907e9
bakito <github@bakito.ch>
2025-09-02 21:23:44 +0200

    update toolbox

--- .toolbox.mk
+++ .toolbox.mk
@@ -8,14 +8,14 @@ TB_LOCALBIN ?= $(TB_LOCALDIR)/bin
 $(TB_LOCALBIN):
 	if [ ! -e $(TB_LOCALBIN) ]; then mkdir -p $(TB_LOCALBIN); fi
 
+# Helper functions
+STRIP_V = $(patsubst v%,%,$(1))
+
 ## Tool Binaries
 TB_DEEPCOPY_GEN ?= $(TB_LOCALBIN)/deepcopy-gen
 TB_GINKGO ?= $(TB_LOCALBIN)/ginkgo
-TB_GOFUMPT ?= $(TB_LOCALBIN)/gofumpt
 TB_GOLANGCI_LINT ?= $(TB_LOCALBIN)/golangci-lint
-TB_GOLINES ?= $(TB_LOCALBIN)/golines
 TB_GORELEASER ?= $(TB_LOCALBIN)/goreleaser
-TB_OAPI_CODEGEN ?= $(TB_LOCALBIN)/oapi-codegen
 TB_SEMVER ?= $(TB_LOCALBIN)/semver
 
 ## Tool Versions
@@ -23,65 +23,45 @@ TB_SEMVER ?= $(TB_LOCALBIN)/semver
 TB_DEEPCOPY_GEN_VERSION ?= v0.34.0
 # renovate: packageName=github.com/onsi/ginkgo/v2
 TB_GINKGO_VERSION ?= v2.25.2
-# renovate: packageName=github.com/mvdan/gofumpt
-TB_GOFUMPT_VERSION ?= v0.8.0
 # renovate: packageName=github.com/golangci/golangci-lint/v2
 TB_GOLANGCI_LINT_VERSION ?= v2.4.0
-# renovate: packageName=github.com/segmentio/golines
-TB_GOLINES_VERSION ?= v0.13.0
+TB_GOLANGCI_LINT_VERSION_NUM ?= $(call STRIP_V,$(TB_GOLANGCI_LINT_VERSION))
 # renovate: packageName=github.com/goreleaser/goreleaser/v2
 TB_GORELEASER_VERSION ?= v2.11.2
-# renovate: packageName=github.com/deepmap/oapi-codegen/v2
-TB_OAPI_CODEGEN_VERSION ?= v2.5.0
 # renovate: packageName=github.com/bakito/semver
 TB_SEMVER_VERSION ?= v1.1.5
 
 ## Tool Installer
 .PHONY: tb.deepcopy-gen
-tb.deepcopy-gen: $(TB_DEEPCOPY_GEN) ## Download deepcopy-gen locally if necessary.
-$(TB_DEEPCOPY_GEN): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/deepcopy-gen || GOBIN=$(TB_LOCALBIN) go install k8s.io/code-generator/cmd/deepcopy-gen@$(TB_DEEPCOPY_GEN_VERSION)
+tb.deepcopy-gen: ## Download deepcopy-gen locally if necessary.
+	@test -s $(TB_DEEPCOPY_GEN) || \
+		GOBIN=$(TB_LOCALBIN) go install k8s.io/code-generator/cmd/deepcopy-gen@$(TB_DEEPCOPY_GEN_VERSION)
 .PHONY: tb.ginkgo
-tb.ginkgo: $(TB_GINKGO) ## Download ginkgo locally if necessary.
-$(TB_GINKGO): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/ginkgo || GOBIN=$(TB_LOCALBIN) go install github.com/onsi/ginkgo/v2/ginkgo@$(TB_GINKGO_VERSION)
-.PHONY: tb.gofumpt
-tb.gofumpt: $(TB_GOFUMPT) ## Download gofumpt locally if necessary.
-$(TB_GOFUMPT): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/gofumpt || GOBIN=$(TB_LOCALBIN) go install mvdan.cc/gofumpt@$(TB_GOFUMPT_VERSION)
+tb.ginkgo: ## Download ginkgo locally if necessary.
+	@test -s $(TB_GINKGO) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/onsi/ginkgo/v2/ginkgo@$(TB_GINKGO_VERSION)
 .PHONY: tb.golangci-lint
-tb.golangci-lint: $(TB_GOLANGCI_LINT) ## Download golangci-lint locally if necessary.
-$(TB_GOLANGCI_LINT): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/golangci-lint || GOBIN=$(TB_LOCALBIN) go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@$(TB_GOLANGCI_LINT_VERSION)
-.PHONY: tb.golines
-tb.golines: $(TB_GOLINES) ## Download golines locally if necessary.
-$(TB_GOLINES): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/golines || GOBIN=$(TB_LOCALBIN) go install github.com/segmentio/golines@$(TB_GOLINES_VERSION)
+tb.golangci-lint: ## Download golangci-lint locally if necessary.
+	@test -s $(TB_GOLANGCI_LINT) && $(TB_GOLANGCI_LINT) --version | grep -q $(TB_GOLANGCI_LINT_VERSION_NUM) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@$(TB_GOLANGCI_LINT_VERSION)
 .PHONY: tb.goreleaser
-tb.goreleaser: $(TB_GORELEASER) ## Download goreleaser locally if necessary.
-$(TB_GORELEASER): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/goreleaser || GOBIN=$(TB_LOCALBIN) go install github.com/goreleaser/goreleaser/v2@$(TB_GORELEASER_VERSION)
-.PHONY: tb.oapi-codegen
-tb.oapi-codegen: $(TB_OAPI_CODEGEN) ## Download oapi-codegen locally if necessary.
-$(TB_OAPI_CODEGEN): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/oapi-codegen || GOBIN=$(TB_LOCALBIN) go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@$(TB_OAPI_CODEGEN_VERSION)
+tb.goreleaser: ## Download goreleaser locally if necessary.
+	@test -s $(TB_GORELEASER) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/goreleaser/goreleaser/v2@$(TB_GORELEASER_VERSION)
 .PHONY: tb.semver
-tb.semver: $(TB_SEMVER) ## Download semver locally if necessary.
-$(TB_SEMVER): $(TB_LOCALBIN)
-	test -s $(TB_LOCALBIN)/semver || GOBIN=$(TB_LOCALBIN) go install github.com/bakito/semver@$(TB_SEMVER_VERSION)
+tb.semver: ## Download semver locally if necessary.
+	@test -s $(TB_SEMVER) || \
+		GOBIN=$(TB_LOCALBIN) go install github.com/bakito/semver@$(TB_SEMVER_VERSION)
 
 ## Reset Tools
 .PHONY: tb.reset
 tb.reset:
 	@rm -f \
-		$(TB_LOCALBIN)/deepcopy-gen \
-		$(TB_LOCALBIN)/ginkgo \
-		$(TB_LOCALBIN)/gofumpt \
-		$(TB_LOCALBIN)/golangci-lint \
-		$(TB_LOCALBIN)/golines \
-		$(TB_LOCALBIN)/goreleaser \
-		$(TB_LOCALBIN)/oapi-codegen \
-		$(TB_LOCALBIN)/semver
+		$(TB_DEEPCOPY_GEN) \
+		$(TB_GINKGO) \
+		$(TB_GOLANGCI_LINT) \
+		$(TB_GORELEASER) \
+		$(TB_SEMVER)
 
 ## Update Tools
 .PHONY: tb.update
@@ -89,10 +69,7 @@ tb.update: tb.reset
 	toolbox makefile --renovate -f $(TB_LOCALDIR)/Makefile \
 		k8s.io/code-generator/cmd/deepcopy-gen@github.com/kubernetes/code-generator \
 		github.com/onsi/ginkgo/v2/ginkgo \
-		mvdan.cc/gofumpt@github.com/mvdan/gofumpt \
-		github.com/golangci/golangci-lint/v2/cmd/golangci-lint \
-		github.com/segmentio/golines \
+		github.com/golangci/golangci-lint/v2/cmd/golangci-lint?--version \
 		github.com/goreleaser/goreleaser/v2 \
-		github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen \
 		github.com/bakito/semver
 ## toolbox - end
