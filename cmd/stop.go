157ac4db0d04e1a586bc8b3c1b69c9c9de8c7a64
Marc Brugger <github@bakito.ch>
2026-09-04 16:57:34 +0200

    feat: add stop --all option (#339)
    Signed-off-by: bakito <github@bakito.ch>

--- cmd/stop.go
+++ cmd/stop.go
@@ -12,34 +12,43 @@ import (
 	"github.com/bakito/gws/internal/tui"
 )
 
-// stopCmd represents the stop command.
-var stopCmd = &cobra.Command{
-	Use:   "stop",
-	Short: "Stop a workstation",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cfg, err := readConfig(args...)
-		if err != nil {
-			return err
-		}
+var (
+	flagStopAll bool
 
-		spinner.Disable()
-		m := tui.NewModel(cmd.Context(), cfg, ">_ GWS Stop", func(ctx context.Context) error {
-			return gcloud.StopWorkstation(ctx, cfg)
-		})
-		m.AutoQuit = true
-
-		p := tea.NewProgram(m)
-		resModel, err := p.Run()
-		if err == nil {
-			log.SetLogger(log.Stdout)
-			if tm, ok := resModel.(*tui.Model); ok {
-				log.Log(tm.LastLog())
+	// stopCmd represents the stop command.
+	stopCmd = &cobra.Command{
+		Use:   "stop",
+		Short: "Stop a workstation",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg, err := readConfig(args...)
+			if err != nil {
+				return err
 			}
-		}
-		return err
-	},
-}
+
+			spinner.Disable()
+			m := tui.NewModel(cmd.Context(), cfg, ">_ GWS Stop", func(ctx context.Context) error {
+				if flagStopAll {
+					return gcloud.StopAllWorkstations(ctx, cfg)
+				}
+				return gcloud.StopWorkstation(ctx, cfg)
+			})
+			m.AutoQuit = true
+
+			p := tea.NewProgram(m)
+			resModel, err := p.Run()
+			if err == nil {
+				log.SetLogger(log.Stdout)
+				if tm, ok := resModel.(*tui.Model); ok {
+					log.Log(tm.LastLog())
+				}
+			}
+			return err
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(stopCmd)
+	stopCmd.PersistentFlags().
+		BoolVar(&flagStopAll, "all", false, "Stop all workstations")
 }
